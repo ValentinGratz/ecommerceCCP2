3bc23cd1016724f4ef3a4da8a4595de94c7387a1
Valentin <valentin.gratz@orange.fr>
2021-01-11 10:15:15 +0100

    push pause

---
+++ src/Controller/CGVController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CGVController extends AbstractController
+{
+    /**
+     * @Route("/c/g/v", name="c_g_v")
+     */
+    public function index(): Response
+    {
+        return $this->render('cgv/index.html.twig', [
+            'controller_name' => 'CGVController',
+        ]);
+    }
+}
