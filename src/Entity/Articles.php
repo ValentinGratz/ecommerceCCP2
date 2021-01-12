ba8db7b7d778ebfe90e40ec528071f340b29704b
Valentin <valentin.gratz@orange.fr>
2021-01-12 08:47:15 +0100

    création entité + mise à jour script bdd

---
+++ src/Entity/Articles.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticlesRepository::class)
+ */
+class Articles
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $ref_article;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $designation_article;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRefArticle(): ?string
+    {
+        return $this->ref_article;
+    }
+
+    public function setRefArticle(string $ref_article): self
+    {
+        $this->ref_article = $ref_article;
+
+        return $this;
+    }
+
+    public function getDesignationArticle(): ?string
+    {
+        return $this->designation_article;
+    }
+
+    public function setDesignationArticle(string $designation_article): self
+    {
+        $this->designation_article = $designation_article;
+
+        return $this;
+    }
+}
