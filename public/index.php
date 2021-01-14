6d428ed25f50974c640708a81eea9ebe8f88a1ff
Valentin <valentin.gratz@orange.fr>
2021-01-14 15:48:47 +0100

    mise en comentaire des lignes mais plus d'affichage

--- public/index.php
+++ public/index.php
@@ -9,14 +9,14 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 //(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+//if ($_SERVER['APP_DEBUG']) {
+ //    umask(0000);
 
-    Debug::enable();
-}
+ //   Debug::enable();
+//}
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+//$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+//$request = Request::createFromGlobals();
+//$response = $kernel->handle($request);
+//$response->send();
+//$kernel->terminate($request, $response);
